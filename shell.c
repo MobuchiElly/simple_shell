648ccff2034431b599dd065c8002888050cc839d
MobuchiElly <ugwueleazer@gmail.com>
2023-04-28 07:27:14 -0700

    Simple Shell

--- shell.c
+++ shell.c
@@ -8,112 +8,36 @@
 
 int main(void)
 {
-	char *input = NULL;
-	size_t input_size = 0;
-	pid_t child_pid;
-	int status;
+	char *text = NULL;
+	size_t text_size = 0;
 
 	while (1)
 	{
-		char *prompt = "#cisfun$ ";
+		char *loop_prompt = "$ ";
 
-		write(STDOUT_FILENO, prompt, _strlen(prompt));
+		write(STDOUT_FILENO, loop_prompt, _strlen(loop_prompt));
 
-		if (getline(&input, &input_size, stdin) == -1)
+		if (getline(&text, &text_size, stdin) == -1)
 		{
 			perror("getline error");
 			exit(EXIT_FAILURE);
 		}
 
-		input[_strcspn(input, "\n")] = '\0';
+		text[_strcspn(text, "\n")] = '\0';
 
-		if (_strcmp(input, "exit") == 0)
+		if (_strcmp(text, "exit") == 0)
 		{
-			free(input);
+			free(text);
 			exit(EXIT_SUCCESS);
 		}
 
-		char *token = strtok(input, " ");
-		char **args = malloc(sizeof(char *) * (MAX_INPUT_SIZE / 2));
-		if (args == NULL)
+		if (system(text) == -1)
 		{
-			perror("malloc error");
+			perror("system");
 			exit(EXIT_FAILURE);
 		}
-
-		int i = 0;
-		while (token != NULL)
-		{
-			args[i] = token;
-			token = strtok(NULL, " ");
-			i++;
-		}
-		args[i] = NULL;
-
-		if (_strcmp(args[0], "cd") == 0)
-		{
-			if (args[1] == NULL)
-			{
-				chdir(_getenv("HOME"));
-			}
-			else
-			{
-				if (chdir(args[1]) == -1)
-				{
-					perror("cd");
-				}
-			}
-		}
-		else if (_strcmp(args[0], "echo") == 0)
-		{
-			for (int j = 1; args[j] != NULL; j++)
-			{
-				write(STDOUT_FILENO, args[j], _strlen(args[j]));
-				if (args[j + 1] != NULL)
-				{
-					write(STDOUT_FILENO, " ", 1);
-				}
-			}
-			write(STDOUT_FILENO, "\n", 1);
-		}
-		else if (_strcmp(args[0], "pwd") == 0)
-		{
-			char *cwd = getcwd(NULL, 0);
-			if (cwd == NULL)
-			{
-				perror("getcwd");
-				exit(EXIT_FAILURE);
-			}
-			write(STDOUT_FILENO, cwd, _strlen(cwd));
-			write(STDOUT_FILENO, "\n", 1);
-			free(cwd);
-		}
-		else
-		{
-			child_pid = fork();
-			if (child_pid == -1)
-			{
-				perror("fork");
-				exit(EXIT_FAILURE);
-			}
-
-			if (child_pid == 0)
-			{
-				if (execvp(args[0], args) == -1)
-				{
-					perror("execvp");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				waitpid(child_pid, &status, 0);
-			}
-		}
-
-		free(args);
 	}
 
-	free(input);
+	free(text);
 	return (0);
 }
